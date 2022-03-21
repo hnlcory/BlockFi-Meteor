23e84ad21004babbfe9fcf03b94f5691de77a03f
Cory-Mac <school.coryparker@gmail.com>
2022-03-20 15:52:28 -1000

    Working, need cleanup

--- app/imports/ui/pages/BlockFi.jsx
+++ app/imports/ui/pages/BlockFi.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu';
 import MidPage from '../components/MidPage';
 import MiddleMenu from '../components/MiddleMenu';
-import FooterMenu from '../components/FooterMenu';
 
 export default class Blockfi extends React.Component {
 
